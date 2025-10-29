9fd637ac61acffbc1626704c92d124ae2a162170
siddiqui-m <muhammad.siddiqui98@hotmail.com>
2025-10-29 16:29:12 +0500

    V0.2

---
+++ Piracy Starter Kit.bat
@@ -0,0 +1,14 @@
+@echo off
+title Sid's Piracy Starter Kit
+cls
+echo.
+echo =======================================
+echo      What would you like to do?
+echo =======================================
+echo          Options:
+echo  1 Set me to be a full pirate.
+echo        This installs all necessary software.
+echo  2 Let me customise my installs.
+echo        Opens more complex menu.
+set /p task=Enter 1 or 2 to continue
+pause
